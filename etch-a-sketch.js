683e0405000db3d24dd5287921c6c586a1be62b3
Akiraz <alec.kirazian@gmail.com>
2021-08-05 22:12:10 -0400

    Added rainbow color

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,11 +4,17 @@ createBox(16);
 const changeButton = document.getElementById("change-button");
 changeButton.addEventListener("click", changeSize);
 
+function getRandomColor() {
+  let randomNumber = Math.floor(Math.random()*16777215);
+  return randomNumber.toString(16);
+}
+
 function createBox(size) {
-  if (size > 100) return alert("Over 100 is too big! Try again.");
+  if (size > 64) return alert("Over 64 is too big! Try again.");
 
   let container = document.getElementById("container");
   container.innerHTML = ""
+
 
   for (let i = 0; i < size; i++) {
     let column = document.createElement("div");
@@ -16,15 +22,12 @@ function createBox(size) {
   
     for (let i = 0; i < size; i++) {
       let row = document.createElement("div");
-      row.id = "row";
-      
+      row.classList.add("row");
       row.addEventListener("mouseenter", () => {
-        row.classList.add("color");
+        row.style.backgroundColor = "#" + getRandomColor();
       });
-  
       column.appendChild(row);
     }
-  
     let container = document.getElementById("container");
     container.appendChild(column);
   }
