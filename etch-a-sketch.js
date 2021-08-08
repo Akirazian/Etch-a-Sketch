681b6ed983be58c73b405b6df1101536662b79b9
Akiraz <alec.kirazian@gmail.com>
2021-08-08 13:04:37 -0400

    Added ability to change backround color

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,14 @@
 const container = document.getElementById("container");
 const sizeValue = document.getElementById("size-value");
 const slider = document.getElementById("slider");
-const blackButton = document.getElementById("black-button");
 const rainbowButton = document.getElementById("rainbow-button");
 const eraserButton = document.getElementById("eraser-button");
 const clearButton = document.getElementById("clear-button");
-const colorPicker = document.getElementById("color-picker");
+const penColorInput = document.getElementById("pen-color");
+const backgroundColorInput = document.getElementById("background-color")
 const toggleGridButton = document.getElementById("toggle-grid");
 
-let color = "black";
+let color = "#333333";
 
 function getRandomColor() {
   let randomNumber = Math.floor(Math.random()*16777215);
@@ -24,19 +24,42 @@ function createBox(size) {
     for (let i = 0; i < size; i++) {
       let row = document.createElement("div");
       row.classList.add("row");
+      row.style.backgroundColor = backgroundColorInput.value;
       if (gridMode === false) row.classList.add("gridOff");
       row.addEventListener("mouseenter", () => {
         if (drawMode === false) return;
         if (color === "rainbow") {
           row.style.backgroundColor = "#" + getRandomColor();
         } else row.style.backgroundColor = color;
+        row.classList.add("colored");
+        if (color === backgroundColorInput.value) { row.classList.remove("colored");}
       });
       column.appendChild(row);
     }
     container.appendChild(column);
- 
   }
 }
+
+let eraserMode = false;
+
+function draw() {
+  eraserMode = false;
+  color = penColorInput.value
+}
+
+function changeBackground() { //change background
+  let grid = document.querySelectorAll(".row");
+  grid.forEach((row) => {
+    if (row.classList.contains("colored")) return;
+    row.style.backgroundColor = backgroundColorInput.value;
+    });
+  if (eraserMode == true) color = backgroundColorInput.value; //if eraser was being used, change the eraser color so it matches new background
+}
+
+function toggleEraser () { 
+  eraserMode = true; 
+  color = backgroundColorInput.value;
+};
 
 let drawMode = true; //Toggle pen
 container.addEventListener("click", () =>{
@@ -62,12 +85,15 @@ function toggleGrid() {
 //Event listeners
 slider.addEventListener("change", () => createBox(slider.value));
 slider.addEventListener("input", () => {sizeValue.innerText = `${slider.value} x ${slider.value}`});
-blackButton.addEventListener("click", () => color = "black");
-rainbowButton.addEventListener("click", () => color = "rainbow");
-eraserButton.addEventListener("click", () => color = "white");
+rainbowButton.addEventListener("click", () => {
+eraserMode = false;
+color = "rainbow";
+});
+eraserButton.addEventListener("click", toggleEraser);
 clearButton.addEventListener("click", () => createBox(slider.value));
-colorPicker.addEventListener("change", () => color = colorPicker.value);
-colorPicker.addEventListener("mouseup", () => color = colorPicker.value); //just in case the user switches back to their chosen color, but doesn't change it!
+penColorInput.addEventListener("change", draw);
+penColorInput.addEventListener("mouseup", draw); //check for mouse up, just in case the user switches back to their chosen color, but doesn't change it!
+backgroundColorInput.addEventListener("input", changeBackground); 
 toggleGridButton.addEventListener("click", toggleGrid);
 
 createBox(16); // start with default size
